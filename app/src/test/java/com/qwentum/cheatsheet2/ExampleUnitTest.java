bf1987437141e64e6ddd721e6ae0f8e637dda9cb
marianbednarik <bednarikmarian154@gmail.com>
2016-05-09 19:25:54 +0200

    Git project cleanup

--- app/src/test/java/com/qwentum/cheatsheet2/ExampleUnitTest.java
+++ app/src/test/java/com/qwentum/cheatsheet2/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.qwentum.cheatsheet2;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
